cbea7578322f9e161c9527442f29a59a52e057cc
Christian A. Schlubach <christian.schlubach@gmail.com>
2014-04-06 23:03:32 -0400

    tweaking functionality

--- spotify/beat.js
+++ spotify/beat.js
@@ -15,7 +15,7 @@ var tracks = [];
 
 // (1) We're going to construct our search using Spotify's API, in our case
 // searching for Kanye West and limiting our results to 10 tracks
-var search = 'Kanye West';
+var search = 'Arcade Fire';
 var maxTracks = 10;
 
 
@@ -85,7 +85,8 @@ function rowAttrsFromTrack(track) {
         // shortcut for writing out a full if/else statement.  You can read
         // more about it at:
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
-        'data-tempo': track.echo ? track.echo.audio_summary.tempo : -1
+        'data-tempo': track.echo ? track.echo.audio_summary.tempo : -1,
+        'data-valence': track.echo.audio_summary.valence
     };
 
     return attrs;
